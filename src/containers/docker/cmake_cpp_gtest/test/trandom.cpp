27fc190a192ddccc81a84c906efb89e0b33b4e8a
Yui Chun Leung <leo.leung@faculty.ai>
2023-04-07 15:15:30 +0100

    update

--- src/containers/docker/cmake_cpp_gtest/test/trandom.cpp
+++ src/containers/docker/cmake_cpp_gtest/test/trandom.cpp
@@ -1,5 +1,8 @@
-#include "random.cpp"
+#include "random.h"
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+using ::testing::AtLeast;   
  
 TEST(SquareRootTest, PositiveNos) { 
     ASSERT_EQ(6, squareRoot(36.0));
@@ -12,6 +15,18 @@ TEST(SquareRootTest, NegativeNos) {
     ASSERT_EQ(-1.0, squareRoot(-15.0));
     ASSERT_EQ(-1.0, squareRoot(-0.2));
 }
+
+// TEST(PainterTest, CanDrawSomething)
+// {
+//     MockTurtle turtle;             // #2
+
+//     EXPECT_CALL(turtle, PenDown()) // #3
+//         .Times(AtLeast(1));
+
+//     Painter painter(&turtle); // #4
+
+//     EXPECT_TRUE(painter.DrawCircle(0, 0, 10));
+// } // #5
  
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);

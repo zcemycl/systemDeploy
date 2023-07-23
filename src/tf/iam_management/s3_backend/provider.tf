b515c27e1159a25a0cf3aa72e97f73167f258ca5
zcemycl <lyc010197@gmail.com>
2023-07-23 18:01:28 +0100

    update s3 backend

--- src/tf/iam_management/s3_backend/provider.tf
+++ src/tf/iam_management/s3_backend/provider.tf
@@ -4,6 +4,14 @@ terraform {
       source = "hashicorp/aws"
     }
   }
+
+ # add after s3 is created
+  backend "s3" {
+    bucket = "leo-systemdeploy-cloudservices-tfstate"
+    key = "global/s3/terraform-backend.tfstate"
+    region = "eu-west-2"
+    encrypt = true
+  }
 }
 
 provider "aws" {

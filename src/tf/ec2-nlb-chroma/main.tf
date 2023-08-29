84490841c996a2d88dfe1e788c091e64f6086f99
zcemycl <lyc010197@gmail.com>
2023-08-29 22:23:31 +0100

    nlb chroma ec2 init

---
+++ src/tf/ec2-nlb-chroma/main.tf
@@ -0,0 +1,60 @@
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "ssh_key"
+  public_key = tls_private_key.ssh_key.public_key_openssh
+}
+
+resource "local_file" "cloud_pem" {
+  filename = "ssh-chroma.pem"
+  content  = tls_private_key.ssh_key.private_key_pem
+
+  provisioner "local-exec" {
+    command = "chmod 600 ssh-chroma.pem"
+  }
+}
+
+resource "aws_vpc" "base_vpc" {
+  cidr_block       = "10.1.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "vpc"
+  }
+}
+
+module "public_access_network" {
+  source                     = "./modules/subnets"
+  vpc_id                     = aws_vpc.base_vpc.id
+  include_public_route_table = true
+}
+
+module "security_groups" {
+  source = "./modules/security_groups"
+  security_groups = [
+    {
+      name        = "ec2-chroma"
+      description = "Chroma instance security group"
+      vpc_id      = aws_vpc.base_vpc.id
+      ingress_rules = [
+        {
+          protocol    = "tcp"
+          from_port   = 8000
+          to_port     = 8000
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+      ]
+      egress_rules = [
+        {
+          protocol    = "-1"
+          from_port   = 0
+          to_port     = 0
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+      ]
+    }
+  ]
+}
---
+++ src/tf/ec2-nlb-chroma/modules/subnets/outs.tf
@@ -0,0 +1,11 @@
+output "igw_group" {
+  value = aws_internet_gateway.igw
+}
+
+output "public_route_tables" {
+  value = aws_route_table.public_route_table
+}
+
+output "private_route_tables" {
+  value = aws_route_table.private_route_table
+}

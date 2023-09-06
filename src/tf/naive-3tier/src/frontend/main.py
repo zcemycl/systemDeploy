1544f7c5a3c3813c9d67b04d26a17809f4b304a3
zcemycl <lyc010197@gmail.com>
2023-09-06 01:08:12 +0100

    update

--- src/tf/naive-3tier/src/frontend/main.py
+++ src/tf/naive-3tier/src/frontend/main.py
@@ -1,7 +1,35 @@
+import os
 from flask import Flask
+import asyncio
+import requests
+import aiohttp
+from loguru import logger
 
+BACKEND_HOST = os.environ["BACKEND_HOST"]
+logger.info(f"Backend Host: {BACKEND_HOST}")
 app = Flask(__name__)
+
+async def fetch_url(session, url):
+    """Fetch the specified URL using the aiohttp session specified."""
+    response = await session.get(url)
+    return {'url': response.url,
+        'status': response.status,
+        "json": await response.json()}
 
 @app.route("/")
 def hello_world():
     return "<h4>Hello World!</h4>"
+
+@app.route("/async")
+async def async_page():
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        task = asyncio.create_task(fetch_url(session, f"{BACKEND_HOST}/async"))
+        tasks.append(task)
+        results = await asyncio.gather(*tasks)
+
+    res = ""
+    for result in results:
+        logger.info(result)
+        res = result['json']['result']
+    return f"<h4>{res}</h4>"

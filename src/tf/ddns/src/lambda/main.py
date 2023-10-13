8e2f0bdfc9bb681563b5a424760d086b2cf888b2
zcemycl <lyc010197@gmail.com>
2023-10-13 23:41:46 +0100

    update

--- src/tf/ddns/src/lambda/main.py
+++ src/tf/ddns/src/lambda/main.py
@@ -1,17 +1,18 @@
 import boto3
 
+BASE_DOMAIN = 'freecaretoday.com'
 allowed_modes = ['set', 'get', 'del']
 r53 = boto3.client('route53')
 
 def lambda_handler(event, context):
-    method = 'set'
-    subdomain = 'aag'
-    ddns_record = f'{subdomain}.freecaretoday.com'
+    method = 'get'
+    subdomain = 'aaj'
+    ddns_record = f'{subdomain}.{BASE_DOMAIN}'
     ip = '84.64.54.43'
     hzones = r53.list_hosted_zones()['HostedZones']
     print(hzones)
     for i in range(len(hzones)):
-        if hzones[i]['Name'] == 'freecaretoday.com.':
+        if hzones[i]['Name'] == f'{BASE_DOMAIN}.':
             id_target = hzones[i]['Id']
             records = r53.list_resource_record_sets(HostedZoneId=id_target)
             print(records['ResourceRecordSets'])
@@ -45,9 +46,42 @@ def lambda_handler(event, context):
             HostedZoneId=id_target.replace('/hostedzone/',''),
         )
         print(resp1)
+        resp2 = r53.change_resource_record_sets(
+            ChangeBatch={
+                'Changes': [
+                    {
+                        'Action': 'CREATE',
+                        'ResourceRecordSet': {
+                            'Name': ddns_record,
+                            'ResourceRecords': [{'Value': ip}],
+                            'TTL': 300,
+                            'Type': 'A',
+                            },
+                        },
+                    ],
+            },
+            HostedZoneId=resp['HostedZone']['Id'].replace('/hostedzone/',''),
+        )
+        print(resp2)
 
         # if hosted zone exists, update only
-
+        resp3 = r53.change_resource_record_sets(
+            ChangeBatch={
+                'Changes': [
+                        {
+                            'Action': 'UPSERT',
+                            'ResourceRecordSet': {
+                                'Name': ddns_record,
+                                'Type': 'A',
+                                'TTL': 300,
+                                'ResourceRecords': [{'Value': '192.168.1.1'}]
+                            }
+                        }
+                ]
+            },
+            HostedZoneId=resp['HostedZone']['Id'].replace('/hostedzone/',''),
+        )
+        print(resp3)
         pass
     elif method == 'get':
         pass

7e506d34f65c9e20fc70baf4bf8003ac1b3597a4
zcemycl <lyc010197@gmail.com>
2023-10-14 08:41:22 +0100

    update dynamodb table

--- src/tf/ddns/main.tf
+++ src/tf/ddns/main.tf
@@ -1,9 +1,7 @@
 resource "aws_dynamodb_table" "dns_record_table" {
   name         = "dns_record_catalog"
   billing_mode = "PAY_PER_REQUEST"
-  # read_capacity    = 1
-  # write_capacity   = 1
-  hash_key = "subdomain_name"
+  hash_key     = "subdomain_name"
   attribute {
     name = "subdomain_name"
     type = "S"
@@ -12,4 +10,24 @@ resource "aws_dynamodb_table" "dns_record_table" {
   #   name = "zone_id"
   #   type = "S"
   # }
+}
+
+resource "aws_dynamodb_table_item" "ddns_record_items" {
+  table_name = aws_dynamodb_table.dns_record_table.name
+  hash_key   = aws_dynamodb_table.dns_record_table.hash_key
+
+  for_each = {
+    "${var.ddns_domain}" = {
+      zone_id = "${aws_route53_zone.ddns_zone.id}"
+    }
+    "${var.domain}" = {
+      zone_id = "${data.aws_route53_zone.base_zone.id}"
+    }
+  }
+  item = <<ITEM
+  {
+    "subdomain_name": {"S": "${each.key}"},
+    "zone_id": {"S": "${each.value.zone_id}"}
+  }
+  ITEM
 }

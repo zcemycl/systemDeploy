7a8beb9f405aa69630e127ce8d70daf9428f223e
zcemycl <lyc010197@gmail.com>
2023-12-16 13:34:06 +0000

    update

--- src/tf/sageMaker/main.tf
+++ src/tf/sageMaker/main.tf
@@ -28,3 +28,16 @@ resource "aws_sagemaker_user_profile" "example" {
   domain_id         = aws_sagemaker_domain.example.id
   user_profile_name = "leo-leung"
 }
+
+resource "aws_sagemaker_app" "example" {
+  domain_id         = aws_sagemaker_domain.example.id
+  user_profile_name = aws_sagemaker_user_profile.example.user_profile_name
+  app_name          = "leo-jplab"
+  app_type          = "JupyterServer"
+}
+
+resource "aws_sagemaker_notebook_instance" "ni" {
+  name          = "leo-notebook-instance"
+  role_arn      = aws_iam_role.sagemaker_role.arn
+  instance_type = "ml.t2.medium"
+}

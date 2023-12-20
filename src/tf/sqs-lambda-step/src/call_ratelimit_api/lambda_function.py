3f1bd438072f6e87b8afeb18a5716bea57949418
zcemycl <lyc010197@gmail.com>
2023-12-20 10:21:59 +0000

    update step function catch error and randomness of lambda_handler

--- src/tf/sqs-lambda-step/src/call_ratelimit_api/lambda_function.py
+++ src/tf/sqs-lambda-step/src/call_ratelimit_api/lambda_function.py
@@ -3,4 +3,6 @@ import random
 
 def lambda_handler(event, context):
     print(event)
+    if random.choices([0,1], weights=[4,6])[0]:
+        raise ValueError("Bad Luck! Queue Again!")
     return event

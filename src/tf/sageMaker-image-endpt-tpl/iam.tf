5d06101be018290e8f501bb76d5e706bee4a3ae5
zcemycl <lyc010197@gmail.com>
2024-01-17 21:37:28 +0000

    update

---
+++ src/tf/sageMaker-image-endpt-tpl/iam.tf
@@ -0,0 +1,70 @@
+# sagemaker
+resource "aws_iam_role" "this" {
+  assume_role_policy = data.aws_iam_policy_document.sagemaker_role.json
+}
+
+data "aws_iam_policy_document" "sagemaker_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["sagemaker.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "sagemaker_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:*",
+      "s3:*"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "sagemaker_policy" {
+  name   = "sagemaker-policy"
+  policy = data.aws_iam_policy_document.sagemaker_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "sagemaker_attach" {
+  role       = aws_iam_role.this.name
+  policy_arn = aws_iam_policy.sagemaker_policy.arn
+}
+
+# lambda
+resource "aws_iam_role" "this_lambda" {
+  assume_role_policy = data.aws_iam_policy_document.lambda_role.json
+}
+
+data "aws_iam_policy_document" "lambda_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "lambda_policy" {
+  statement {
+    effect    = "Allow"
+    actions   = ["sagemaker:*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name   = "lambda-policy"
+  policy = data.aws_iam_policy_document.lambda_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_attach" {
+  role       = aws_iam_role.this_lambda.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
---
+++ src/tf/sageMaker-image-endpt-tpl/main.tf
@@ -0,0 +1,90 @@
+locals {
+  prefix              = "dummy-model-image-2"
+  lambda_zip_location = "outputs/lambda_function.zip"
+}
+
+data "archive_file" "lambda_function" {
+  type        = "zip"
+  source_file = "src/lambda-invoke/lambda_function.py"
+  output_path = local.lambda_zip_location
+}
+
+# data "archive_file" "dummy_model" {
+#   type        = "tar"
+#   source_file = "src/lambda-invoke/lambda_function.py"
+#   output_path = "outputs/model.tar"
+# }
+
+resource "aws_s3_bucket" "this" {
+  bucket        = "${local.prefix}-bucket"
+  force_destroy = false
+}
+
+resource "aws_s3_bucket_object" "this" {
+  bucket        = aws_s3_bucket.this.bucket
+  key           = "model.tar.gz"
+  source        = "outputs/model.tar.gz"
+  force_destroy = false
+}
+
+resource "aws_ecr_repository" "this" {
+  name         = local.prefix
+  force_delete = true
+}
+
+resource "null_resource" "push_docker_image" {
+  provisioner "local-exec" {
+    command = "aws ecr get-login-password --region ${var.AWS_REGION} | docker login --username AWS --password-stdin ${aws_ecr_repository.this.registry_id}.dkr.ecr.${var.AWS_REGION}.amazonaws.com >> info.txt"
+  }
+  provisioner "local-exec" {
+    command = "docker tag test-dummy-sagemaker-image:latest ${aws_ecr_repository.this.repository_url}:latest >> info.txt"
+  }
+  provisioner "local-exec" {
+    command = "docker push ${aws_ecr_repository.this.repository_url}:latest >> info.txt"
+  }
+  depends_on = [
+    aws_ecr_repository.this
+  ]
+}
+
+resource "aws_sagemaker_model" "this" {
+  name               = "${local.prefix}-sagemaker-model"
+  execution_role_arn = aws_iam_role.this.arn
+
+  primary_container {
+    image          = "${aws_ecr_repository.this.repository_url}:latest"
+    mode           = "SingleModel"
+    model_data_url = "s3://dummy-model-image-bucket/model.tar.gz"
+  }
+}
+
+resource "aws_sagemaker_endpoint_configuration" "this" {
+  name = "${local.prefix}-sagemaker-conf"
+
+  production_variants {
+    variant_name           = "default"
+    model_name             = aws_sagemaker_model.this.name
+    initial_instance_count = 1
+    instance_type          = "ml.t2.medium"
+  }
+}
+
+resource "aws_sagemaker_endpoint" "this" {
+  name                 = "${local.prefix}-sagemaker-endpt"
+  endpoint_config_name = aws_sagemaker_endpoint_configuration.this.name
+}
+
+resource "aws_lambda_function" "this" {
+  filename         = local.lambda_zip_location
+  function_name    = "${local.prefix}-sagemaker-invoke"
+  handler          = "lambda_function.lambda_handler"
+  source_code_hash = data.archive_file.lambda_function.output_base64sha256
+  runtime          = "python3.10"
+  role             = aws_iam_role.this_lambda.arn
+
+  environment {
+    variables = {
+      endpoint_name = aws_sagemaker_endpoint.this.name
+    }
+  }
+}

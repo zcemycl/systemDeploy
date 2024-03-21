4cb98e82e69fee15311831d21138ba649e677273
zcemycl <lyc010197@gmail.com>
2024-03-21 22:21:38 +0000

    update

--- src/tf/cognito-app/frontend/src/app/page.tsx
+++ src/tf/cognito-app/frontend/src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1>Hello World!</h1>
+  return (
+    <div className="place-content-center justify-center text-center">
+      Hello World!
+    </div>
+  );
 }
---
+++ src/tf/cognito-app/frontend/src/components/navbar/index.tsx
@@ -0,0 +1,7 @@
+export default function NavBar() {
+    return (
+        <h1 className="place-content-center">
+            NavBar
+        </h1>
+    )
+}

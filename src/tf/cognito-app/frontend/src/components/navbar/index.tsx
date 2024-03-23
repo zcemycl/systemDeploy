b39fa976964d705cd0ac41263af2bbe180bf43ad
zcemycl <lyc010197@gmail.com>
2024-03-23 10:58:12 +0000

    update

--- src/tf/cognito-app/frontend/src/components/navbar/index.tsx
+++ src/tf/cognito-app/frontend/src/components/navbar/index.tsx
@@ -145,7 +145,7 @@ export default function NavBar() {
         </div>
         <div className="flex w-full justify-end h-0">
           <div
-            className={`items-center justify-between w-1/4 md:hidden transition-transform ${isDropdownOpen ? "scale-y-100" : "scale-y-0"}`}
+            className={`items-center justify-between w-1/3 sm:w-1/4 md:hidden transition-transform ${isDropdownOpen ? "scale-y-100" : "scale-y-0"}`}
             id="navbar-dropdown"
           >
             <ul className="flex flex-col p-2 md:p-0 mt-1 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">

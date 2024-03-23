cff7faba503243f18fc564f6b489d2c0279676a3
zcemycl <lyc010197@gmail.com>
2024-03-23 10:20:59 +0000

    add transition

--- src/tf/cognito-app/frontend/src/components/navbar/index.tsx
+++ src/tf/cognito-app/frontend/src/components/navbar/index.tsx
@@ -143,31 +143,29 @@ export default function NavBar() {
             </button>
           </div>
         </div>
-        {isDropdownOpen && (
-          <div className="flex w-full justify-end">
-            <div
-              className="items-center justify-between w-1/4 md:hidden"
-              id="navbar-dropdown"
-            >
-              <ul className="flex flex-col p-2 md:p-0 mt-1 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                <li>
-                  {navbar_dropdown.map((keyValue) => {
-                    return (
-                      <Link
-                        href={keyValue.path}
-                        key={keyValue.name}
-                        className="block py-1 px-3 text-right text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                        aria-current="page"
-                      >
-                        {keyValue.name}
-                      </Link>
-                    );
-                  })}
-                </li>
-              </ul>
-            </div>
+        <div className="flex w-full justify-end h-0">
+          <div
+            className={`items-center justify-between w-1/4 md:hidden transition-transform ${isDropdownOpen ? "scale-y-100" : "scale-y-0"}`}
+            id="navbar-dropdown"
+          >
+            <ul className="flex flex-col p-2 md:p-0 mt-1 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                {navbar_dropdown.map((keyValue) => {
+                  return (
+                    <Link
+                      href={keyValue.path}
+                      key={keyValue.name}
+                      className="block py-1 px-3 text-right text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                      aria-current="page"
+                    >
+                      {keyValue.name}
+                    </Link>
+                  );
+                })}
+              </li>
+            </ul>
           </div>
-        )}
+        </div>
       </div>
     </header>
   );

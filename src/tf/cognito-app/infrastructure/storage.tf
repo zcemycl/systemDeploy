8af69cde07497983a09cf7794b953a5bcfb699d3
zcemycl <lyc010197@gmail.com>
2024-04-01 23:21:22 +0100

    update

--- src/tf/cognito-app/infrastructure/storage.tf
+++ src/tf/cognito-app/infrastructure/storage.tf
@@ -4,6 +4,12 @@ locals {
     "define_auth_challenge" : {}
     "verify_auth_challenge" : {}
   }
+  s3_data_etl_folder = {
+    raw : {}
+    bronze : {}
+    silver : {}
+    gold : {}
+  }
 }
 
 data "archive_file" "this_lambda" {
@@ -25,4 +31,16 @@ resource "aws_s3_bucket_object" "this" {
   source        = data.archive_file.this_lambda[each.key].output_path
   force_destroy = true
   etag          = data.archive_file.this_lambda[each.key].output_md5
+}
+
+resource "aws_s3_bucket" "this_data" {
+  bucket        = "${var.project_name}-data-bucket"
+  force_destroy = true
+}
+
+resource "aws_s3_object" "this_data_folder" {
+  for_each     = local.s3_data_etl_folder
+  bucket       = aws_s3_bucket.this.id
+  key          = each.key
+  content_type = "application/x-directory"
 }

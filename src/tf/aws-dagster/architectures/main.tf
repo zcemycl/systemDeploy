e9c756af40a7792046588d5ee8847fa1426a238f
zcemycl <lyc010197@gmail.com>
2024-07-12 00:03:09 +0100

    generalise dagster webserver and daemon conf

--- src/tf/aws-dagster/architectures/main.tf
+++ src/tf/aws-dagster/architectures/main.tf
@@ -49,7 +49,43 @@ module "ecr" {
 
   images = [
     {
-      name                 = "${var.prefix}-code-server"
+      name                 = "${var.prefix}-etl1-code-server"
+      lastn                = 5
+      image_tag_mutability = "MUTABLE"
+      scan_on_push         = true
+      force_delete         = true
+      enable_vpc_endpt     = false
+      vpc_id               = aws_vpc.this.id
+      sg_ids               = []
+      subnet_ids           = []
+      route_table_ids      = []
+    },
+    {
+      name                 = "${var.prefix}-etl2-code-server"
+      lastn                = 5
+      image_tag_mutability = "MUTABLE"
+      scan_on_push         = true
+      force_delete         = true
+      enable_vpc_endpt     = false
+      vpc_id               = aws_vpc.this.id
+      sg_ids               = []
+      subnet_ids           = []
+      route_table_ids      = []
+    },
+    {
+      name                 = "${var.prefix}-dagster-webserver"
+      lastn                = 5
+      image_tag_mutability = "MUTABLE"
+      scan_on_push         = true
+      force_delete         = true
+      enable_vpc_endpt     = false
+      vpc_id               = aws_vpc.this.id
+      sg_ids               = []
+      subnet_ids           = []
+      route_table_ids      = []
+    },
+    {
+      name                 = "${var.prefix}-dagster-daemon"
       lastn                = 5
       image_tag_mutability = "MUTABLE"
       scan_on_push         = true

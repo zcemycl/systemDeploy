6bce70474cbb135dcd66ded3efbb301389a98b40
zcemycl <lyc010197@gmail.com>
2024-07-12 21:33:58 +0100

    update

--- src/tf/aws-dagster/codelocations/etl1/etl1/__init__.py
+++ src/tf/aws-dagster/codelocations/etl1/etl1/__init__.py
@@ -1,28 +1,39 @@
+import os
+
 from dagster import AssetExecutionContext, Definitions, asset, define_asset_job
 from dagster_aws.s3 import S3PickleIOManager, S3Resource
+
+ENV = os.getenv("ENV", "local")
+match ENV:
+    case "dev":
+        bucket_name = "aws-dagster-storage"
+        default_s3_resource = S3Resource(region_name="eu-west-2")
+    case _:
+        bucket_name = "my-bucket"
+        default_s3_resource = S3Resource(
+            region_name="eu-west-2",
+            endpoint_url="http://dagster-s3:9000",
+            aws_secret_access_key="minioadmin",
+            aws_access_key_id="minioadmin",
+        )
 
 
 @asset
 def asset1():
-    return [1,2]
+    return [1, 2]
+
 
 @asset
 def asset2(context: AssetExecutionContext, s3: S3Resource):
     objs = s3.get_client().list_objects_v2(
-        Bucket='my-bucket',
-        Prefix='some-key'
+        Bucket=bucket_name, Prefix="some-key"
     )
     context.log.info(objs)
-    return [3,4]
+    return [3, 4]
+
 
 # jobs = define_asset_job("all_assets", selection=[asset1,asset2])
 
-default_s3_resource = S3Resource(
-    region_name="eu-west-2",
-    endpoint_url="http://dagster-s3:9000",
-    aws_secret_access_key="minioadmin",
-    aws_access_key_id="minioadmin"
-    )
 
 defs = Definitions(
     assets=[asset1, asset2],
@@ -30,8 +41,7 @@ defs = Definitions(
     resources={
         "s3": default_s3_resource,
         "io_manager": S3PickleIOManager(
-            s3_resource=default_s3_resource,
-            s3_bucket="my-bucket"
-        )
-    }
+            s3_resource=default_s3_resource, s3_bucket=bucket_name
+        ),
+    },
 )
--- src/tf/aws-dagster/codelocations/etl2/etl2/__init__.py
+++ src/tf/aws-dagster/codelocations/etl2/etl2/__init__.py
@@ -1,18 +1,26 @@
+import os
+
 from dagster import AssetExecutionContext, Definitions, asset, define_asset_job
 from dagster_aws.s3 import S3PickleIOManager, S3Resource
+
+ENV = os.getenv("ENV", "local")
+match ENV:
+    case "dev":
+        bucket_name = "aws-dagster-storage"
+        default_s3_resource = S3Resource(region_name="eu-west-2")
+    case _:
+        bucket_name = "my-bucket"
+        default_s3_resource = S3Resource(
+            region_name="eu-west-2",
+            endpoint_url="http://dagster-s3:9000",
+            aws_secret_access_key="minioadmin",
+            aws_access_key_id="minioadmin",
+        )
 
 
 @asset
 def asset3():
-    return [1,2]
-
-
-default_s3_resource = S3Resource(
-    region_name="eu-west-2",
-    endpoint_url="http://dagster-s3:9000",
-    aws_secret_access_key="minioadmin",
-    aws_access_key_id="minioadmin"
-    )
+    return [1, 2]
 
 
 defs = Definitions(
@@ -21,8 +29,7 @@ defs = Definitions(
     resources={
         "s3": default_s3_resource,
         "io_manager": S3PickleIOManager(
-            s3_resource=default_s3_resource,
-            s3_bucket="my-bucket"
-        )
-    }
+            s3_resource=default_s3_resource, s3_bucket=bucket_name
+        ),
+    },
 )

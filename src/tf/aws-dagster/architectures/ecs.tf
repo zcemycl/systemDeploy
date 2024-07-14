87a809a1258d3543de4b6bb9a9ca002704849e95
zcemycl <lyc010197@gmail.com>
2024-07-14 12:05:31 +0100

    update

--- src/tf/aws-dagster/architectures/ecs.tf
+++ src/tf/aws-dagster/architectures/ecs.tf
@@ -71,7 +71,7 @@ resource "aws_ecs_capacity_provider" "this" {
 
   auto_scaling_group_provider {
     auto_scaling_group_arn         = aws_autoscaling_group.this.arn
-    managed_termination_protection = "ENABLED"
+    managed_termination_protection = "DISABLED"
 
     managed_scaling {
       maximum_scaling_step_size = 1

4ad965f01163dd8d72b253ea1db61542ebcf69cc
zcemycl <lyc010197@gmail.com>
2025-11-13 17:01:36 +0000

    update

--- src/containers/docker/graphql/backend/app/grpc/services/member.py
+++ src/containers/docker/graphql/backend/app/grpc/services/member.py
@@ -1,2 +1,9 @@
 import app.grpc.generated.member_pb2 as mpb2
 import app.grpc.generated.member_pb2_grpc as mpb2g
+import grpc
+from app.core.database import async_session
+
+
+class MemberService(mpb2g.MemberServiceServicer):
+    def __init__(self):
+        self.async_session = async_session
